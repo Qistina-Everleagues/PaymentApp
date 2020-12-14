8c543c01281361b43ddaba17ea732dfbf8a9ad51
Qistina-Everleagues <qistina.mahmud@everleagues.com>
2020-12-14 17:13:22 +0800

    Added distances in forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailsFormComponent } from './payment-details/payment-details-form/payment-details-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailsFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
